12092ec318f3b443a1925fa41eb39838f7249c8e
michaelrotolo <54121246+michaelrotolo@users.noreply.github.com>
2019-08-17 13:35:04 -0400

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,15 +1,19 @@
 $(function(){
     if ($("#cv-link").hasClass("current")) {
-        $("#about-link").removeClass("current");
+        $("#about-link").removeClass("current-white");
+        $("#about-link").addClass("current");
     }
     if ($("#research-link").hasClass("current")) {
-        $("#about-link").removeClass("current");
+        $("#about-link").removeClass("current-white");
+        $("#about-link").addClass("current");
     }
     if ($("#book-link").hasClass("current")) {
-        $("#about-link").removeClass("current");
+        $("#about-link").removeClass("current-white");
+        $("#about-link").addClass("current");
     }
     if ($("#contact-link").hasClass("current")) {
-        $("#about-link").removeClass("current");
+        $("#about-link").removeClass("current-white");
+        $("#about-link").addClass("current");
     }
     
  });
