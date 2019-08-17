6e1a77d034c2be8b6e5ea165a190d559c89bbe7d
michaelrotolo <54121246+michaelrotolo@users.noreply.github.com>
2019-08-17 14:39:38 -0400

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,7 +1,4 @@
 $(function(){
-    if ($("#cv-link,#research-link,#book-link,#contact-link").not($(".current"))) {
-        $("#about-link").removeClass("current-white");
-        $("#about-link").addClass("current");
-    }
+
     
  });
